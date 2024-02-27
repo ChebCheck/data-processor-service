61b5b168555fee095119424bc97f1e7d3195cef1
wWcheburekWw <gapdanil@yandex.ru>
2024-02-27 06:18:48 +0300

    DI extension added

---
+++ DataProcessor/DI/ServiceRgister.cs
@@ -0,0 +1,21 @@
+﻿using DataProcessor.Entities;
+using DataProcessor.Repositories;
+using DataProcessor.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace DataProcessor.DI;
+
+public static class ServiceRgister
+{
+    public static void AddServices(this IServiceCollection services, string connectionString)
+    {
+        services.AddDbContext<ApplicationContext>((options) =>
+        {
+            //options.UseSqlite(connectionString);
+            options.UseNpgsql(connectionString);
+        });
+        services.AddScoped<IRepository<ModuleStatusEntity>, ModulelStatusRepository>();
+    }
+}
