0450ca12acdd5b88d47d6823ab4a06d620cf8726
wWcheburekWw <gapdanil@yandex.ru>
2024-02-27 06:18:32 +0300

    IRepository created

---
+++ DataProcessor/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace DataProcessor.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<T> Get(string id);
+    Task<IEnumerable<T>> GetAll();
+    Task<T> Create(T entity);
+    Task<T> Update(T entity);
+    Task Delete(string id);
+}
