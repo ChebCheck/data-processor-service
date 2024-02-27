7fb9a034797f98ad5c40e92c5a24bc12643afc80
wWcheburekWw <gapdanil@yandex.ru>
2024-02-27 06:20:45 +0300

    Repository created

--- DataProcessor/Program.cs
+++ DataProcessor/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+using DataProcessor.Models;
+using Microsoft.Extensions.DependencyInjection;
+using DataProcessor.DI;
+using DataProcessor.Interfaces;
+using DataProcessor.Entities;
+
+ServiceCollection services = new ServiceCollection();
+services.AddServices("Host = localhost; Port = 5432; Database = moduleStatus; Username = postgres; Password = postgres"); //"Data Source=moduleStatus.db"
+
+var serviceProvider = services.BuildServiceProvider();
+
+
+Console.WriteLine(" [*] Waiting for messages.");
+InstrumentStatus instrumentStatus = new InstrumentStatus();
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("InstrumentStatus", false, false, false, null);
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    var recivedObject = JsonSerializer.Deserialize<InstrumentStatus>(message);
+    var repository = serviceProvider.GetRequiredService<IRepository<ModuleStatusEntity>>();
+
+    if (recivedObject == null)
+        throw new Exception();
+
+    foreach(var item in recivedObject.DeviceStatuses)
+    {
+        repository.Create(new ModuleStatusEntity()
+        {
+            ModuleCategoryID = item.ModuleCategoryID,
+            ModuleState = item.RapidControlStatus.ModuleState
+        }).Wait();
+    }
+};
+channel.BasicConsume("InstrumentStatus", true, consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ DataProcessor/Repositories/ModulelStatusRepository.cs
@@ -0,0 +1,66 @@
+﻿using DataProcessor.Entities;
+using DataProcessor.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataProcessor.Repositories;
+
+public class ModulelStatusRepository : IRepository<ModuleStatusEntity>
+{
+    private readonly ApplicationContext _context;
+    private readonly DbSet<ModuleStatusEntity> _set;
+
+    public ModulelStatusRepository(ApplicationContext context)
+    {
+        _context = context;
+        _set = context.ModuleStates;
+    }
+
+    public async Task<ModuleStatusEntity> Create(ModuleStatusEntity entity)
+    {
+        var ent = await Get(entity.ModuleCategoryID);
+        if(ent == null)
+        {
+            _set.Add(entity);
+            await _context.SaveChangesAsync();
+        }
+        else
+        {
+            await Update(entity);
+        }
+        
+        return entity;
+    }
+
+    public async Task Delete(string id)
+    {
+        var entity = await Get(id);
+        if (entity != null)
+        {
+            _set.Remove(entity);
+            await _context.SaveChangesAsync();
+        }        
+    }
+
+    public async Task<ModuleStatusEntity?> Get(string id)
+    {
+        return await _set.FirstOrDefaultAsync(e => e.ModuleCategoryID == id);
+    }
+
+    public async Task<IEnumerable<ModuleStatusEntity>> GetAll()
+    {
+        return await _set.ToListAsync();
+    }
+
+    public async Task<ModuleStatusEntity> Update(ModuleStatusEntity entity)
+    {
+        var ent = await Get(entity.ModuleCategoryID);
+        
+        if (ent == null)
+            throw new Exception();
+        
+        ent.ModuleState = entity.ModuleState;
+
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+}
